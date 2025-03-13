261a810df2b10bca608e7238314e66fc685c8381
Myszexx <michalr550@gmail.com>
2025-03-13 16:23:33 +0100

    Add new views and update URL patterns for ZPNs, Leagues, Table, and Fixtures data handling

--- apps/integrations/urls.py
+++ apps/integrations/urls.py
@@ -1,12 +1,16 @@
 
 from django.urls import path, include
 
-from apps.integrations.views import NinetyMinsListV
+from apps.integrations.views import ZPNsListV, LeaguesListV, TableListV, FixturesListV
 
 # from apps.integrations.views import ZPNsListV
 
 urlpatterns = [
 #90mins scrapper
-    path('90mins/', NinetyMinsListV.as_view(), name='NinetyMinsData'),
+    # path('90mins/', NinetyMinsListV.as_view(), name='NinetyMinsData'),
+    path('90mins/zpn/',ZPNsListV.as_view(), name='ZPNsData'),
+    path('90mins/leagues/',LeaguesListV.as_view(), name='LeaguesData'),
+    path('90mins/table/',TableListV.as_view(), name='TableData'),
+    path('90mins/fixtures/',FixturesListV.as_view(), name='FixturesData'),
 
 ]
--- apps/integrations/views.py
+++ apps/integrations/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -13,98 +14,100 @@ from apps.integrations.serializers import ZPNsSerializer, LeaguesSerializer, Tab
 
 # Create your views here.
 
-# class ZPNsListV(generics.RetrieveAPIView):
-#     serializer_class = LinkedListSerializer
-#
-#     def get(self, request):
-#         data = grcp().get_data('/ligireg.html')
-#         if not LinkedListSerializer(data).is_valid():
-#             return Response(status=HTTP_204_NO_CONTENT)
-#
-#         return Response(data, status=HTTP_200_OK)
+class ZPNsListV(generics.ListCreateAPIView):
+    queryset = ZPNs.objects.all()
+    serializer_class = ZPNsSerializer
 
-class NinetyMinsListV(generics.ListCreateAPIView):
-    permission_classes = [AllowAny]
+class LeaguesListV(generics.ListCreateAPIView):
+    queryset = Leagues.objects.all()
+    serializer_class = LeaguesSerializer
 
-
-    def get(self, request, *args, **kwargs):
-        print(**kwargs)
-        type = request.query_params.get('type')
-        filter = request.query_params.get('filter')
-        queryset = None
-        match type:
-            case 'ZPNs':
-                queryset = ZPNs.objects.all()
-                serializer = ZPNsSerializer(queryset, many=True)
-            case 'Leagues':
-                queryset = Leagues.objects.filter(zpn__name__contains=filter)
-                serializer = LeaguesSerializer(queryset, many=True)
-            case 'Tables':
-                queryset = Table.objects.filter(league__table__name__contains=filter)
-                serializer = TableSerializer(queryset, many=True)
-            case 'Fixtures':
-                queryset = Fixtures.objects.filter(Q(home_team__icontains=filter)|Q(away_team__icontains=filter))
-                serializer = FixturesSerializer(queryset, many=True)
-        return Response(serializer.data, status=HTTP_200_OK)
+    def get_queryset(self):
+        queryset = Leagues.objects.all()
+        zpn = self.request.query_params.get('zpn', None)
+        if zpn is not None:
+            queryset = queryset.filter(zpn__name__contains=filter)
+        return queryset
 
     def create(self, request, *args, **kwargs):
         data = request.data['data']
-        type = request.data['type']
         is_good = True
+        for item in data:
+            print(item)
+            zpn_id = ZPNs.objects.filter(name=item['zpn']).first().id
+            season = item['season'] if 'season' in item.keys() else get_current_season()
+            league = {
+                'name': item['name'],
+                'url': item['url'],
+                'zpn_id': zpn_id,
+                'season': season
+            }
+            serializer = LeaguesSerializer(data=item)
+            if serializer.is_valid():
+                Leagues.objects.update_or_create(**league)
+            else:
+                is_good = False
 
-        match type:
-            case 'ZPNs':
+        if not is_good:
+            return Response(status=HTTP_206_PARTIAL_CONTENT)
+        return Response(status=HTTP_200_OK)
 
-                for item in data:
-                    print(item)
-                    serializer = ZPNsSerializer(data=item)
-                    if serializer.is_valid():
-                        print('valid')
-                        serializer.save()
-                    else:
-                        is_good = False
-                if not is_good:
-                    return Response(status=HTTP_206_PARTIAL_CONTENT)
-                return Response(status=HTTP_200_OK)
-            case 'Leagues':
-                for item in data:
-                    print(item)
-                    zpn_id = ZPNs.objects.filter(name=item['zpn']).first().id
-                    season = item['season'] if 'season' in item.keys() else get_current_season()
-                    league = {
-                        'name': item['name'],
-                        'url': item['url'],
-                        'zpn_id': zpn_id,
-                        'season': season
-                    }
-                    serializer = LeaguesSerializer(data=item)
-                    if serializer.is_valid():
-                        Leagues.objects.update_or_create(**league)
-                    else:
-                        is_good = False
-            case 'Tables':
-                for item in data:
-                    league_id = Leagues.objects.filter(name=item['league']).first().id
-                    table = {
-                        'league_id': league_id,
-                        'team_name': item['name'],
-                        'team_url': item['url'],
-                        'matches': item['wins']+item['draws']+item['loses'],
-                        'wins': item['wins'],
-                        'draws': item['draws'],
-                        'losses': item['loses'],
-                        'goals_scored': item['goals_shot'],
-                        'goals_conceded': item['goals_conceded'],
-                        'points': item['points'],
-                        'position': item['standing']
-                    }
-                    serializer = TableSerializer(data=table)
-                    if serializer.is_valid():
-                        Table.objects.update_or_create(**table)
-                    else:
-                        is_good = False
-            case 'Fixtures':
-                for item in data:
+
+
+class TableListV(generics.ListCreateAPIView):
+    queryset = Table.objects.all()
+    serializer_class = TableSerializer
+
+    def get_queryset(self):
+        queryset = Table.objects.all()
+        league = self.request.query_params.get('league', None)
+        if league is not None:
+            queryset = queryset.filter(league__table__name__contains=filter)
+        return queryset
+
+    def create(self, request, *args, **kwargs):
+        data = request.data['data']
+        is_good = True
+        for item in data:
+            league_id = Leagues.objects.filter(name=item['league']).first().id
+            table = {
+                'league_id': league_id,
+                'team_name': item['name'],
+                'team_url': item['url'],
+                'matches': item['wins'] + item['draws'] + item['loses'],
+                'wins': item['wins'],
+                'draws': item['draws'],
+                'losses': item['loses'],
+                'goals_scored': item['goals_shot'],
+                'goals_conceded': item['goals_conceded'],
+                'points': item['points'],
+                'position': item['standing']
+            }
+            serializer = TableSerializer(data=table)
+            if serializer.is_valid():
+                Table.objects.update_or_create(**table)
+            else:
+                is_good = False
+
+        if not is_good:
+            return Response(status=HTTP_206_PARTIAL_CONTENT)
+        return Response(status=HTTP_200_OK)
+
+class FixturesListV(generics.ListCreateAPIView):
+    queryset = Fixtures.objects.all()
+    serializer_class = FixturesSerializer
+
+    def get_queryset(self):
+        queryset = Fixtures.objects.all()
+        team = self.request.query_params.get('team', None)
+        if team is not None:
+            queryset = queryset.filter(Q(home_team__icontains=filter)|Q(away_team__icontains=filter))
+        return queryset
+
+    def create(self, request, *args, **kwargs):
+        data = request.data['data']
+        is_good = True
+        for item in data:
                     league_id = Leagues.objects.filter(name=item['league']).first().id
                     fixture = {
                         'league_id': league_id,
@@ -119,9 +122,7 @@ class NinetyMinsListV(generics.ListCreateAPIView):
                     else:
                         is_good = False
 
-
-
-
         if not is_good:
             return Response(status=HTTP_206_PARTIAL_CONTENT)
         return Response(status=HTTP_200_OK)
+
